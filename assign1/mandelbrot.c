ccffbfb9bdd242349c9ffdba75333426f13c792d
kluxa <31541758+kluxa@users.noreply.github.com>
2017-09-10 11:02:22 +1000

    Update mandelbrot.c

--- assign1/mandelbrot.c
+++ assign1/mandelbrot.c
@@ -1,11 +1,12 @@
 // Assignment 1 Mandelbrot
 //
 // Completed by
-//  ... (z0000000)
-//  ... (z0000000)
+//  Kevin Luxa (z5074984)
+//  Ahnaf Aaqif (z5125723)
+//  Seokhyun Phil Jung (z3459079)
 //
-// Modified on 2017-??-??
-// Tutor's name (dayHH-lab)
+// Modified on 2017-09-08
+// Tutor: Stephen Leung (fri09-kora)
 
 // Add your own #includes here
 
@@ -13,6 +14,9 @@
 #include "pixelColor.h"
 
 // Add your own #defines here
+#define BOUNDARY 2
+#define BOUNDARY_SQUARED BOUNDARY * BOUNDARY
+#define BASE_PIXEL_LENGTH 2
 
 // Add your own function prototypes here
 static complex complexSum(complex c1, complex c2);
@@ -52,16 +56,57 @@ int escapeSteps(complex c) {
 // Fill a grid of TILE_SIZE by TILE_SIZE pixels, with the number of
 // steps each pixel took to escape the Mandelbrot set.
 void escapeGrid(int grid[TILE_SIZE][TILE_SIZE],
-        complex center, int z) {
-
-    // TODO: COMPLETE THIS FUNCTION
-
+                complex center, int z) {
+    double pixelLength = getPixelLength (z);
+    int y = 0;
+    while (y < TILE_SIZE) {
+        int x = 0;
+        while (x < TILE_SIZE) {
+            complex pixelCoords = getCoords (x, y center, pixelLength);
+            grid[y][x] = escapeSteps (pixelCoords);
+            x++;
+        }
+        y++;
+    }
 }
 
 // Add your own functions here.
 // Remember to make them static.
-static complex complexSum(complex c1, complex c2) {
-    complex sum = {
+
+// Takes in two array indices, the length of a pixel, and the center of
+// the tile, and returns the coordinates in the complex plane (given by
+// a complex number) corresponding to the array indices
+static complex getCoords (int x, int y, complex center,
+                          double pixelLength) {
+    // Translates x and y so (0, 0) is the center of the tile
+    x -= TILE_SIZE / 2;
+    y -= TILE_SIZE / 2;
+    // Gets relative (to the center) and absolute coordinates
+    complex relativeCoords = {x * pixelLength,  y * pixelLength};
+    complex absoluteCoords = complexSum (center, relativeCoords);
+    return absoluteCoords;
+}
+
+// Determines the length of a pixel given a zoom level
+// Pixel length = 2^-z = 1 / 2^z
+static double getPixelLength (z) {
+    return (1 / getPower (BASE_PIXEL_LENGTH, z));
+}
+
+// Takes a base and exponent and returns base^exponent
+static unsigned long getPower (int base, int exponent) {
+    int result = 1;
+    int i = 0;
+    while (i < exponent) {
+        result *= base;
+        i++;
+    }
+    return result;
+}
+
+// Adds two complex numbers
+static complex complexSum (complex c1, complex c2) {
+    complex sum =
         .re = c1.re + c2.re,
         .im = c1.im + c2.im
     };
